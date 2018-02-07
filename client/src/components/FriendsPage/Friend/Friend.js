3844c1236130ad343cf0509554e5cf8694db6fc4
Wasif Zaman <teatocode@gmail.com>
2018-02-07 00:54:01 -0500

    popover v1

--- client/src/components/FriendsPage/Friend/Friend.js
+++ client/src/components/FriendsPage/Friend/Friend.js
@@ -6,6 +6,9 @@ import SVGInline from 'react-svg-inline'
 // styles
 import './Friend.scss'
 
+// component
+import Button from '../../Button/Button'
+
 // mappers
 import { mapDispatch } from '../../../mappers/friends.mapper'
 
@@ -14,7 +17,8 @@ class Friend extends Component {
     id: null,
     name: '',
     email: '',
-    status: ''
+    status: '',
+    buttonShown: false
   }
 
   componentWillReceiveProps = nextProps => {
@@ -27,14 +31,42 @@ class Friend extends Component {
     this.setState({ id, name, email, status })
   }
 
+  showButton = () => {
+    this.setState({ buttonShown: true })
+
+    document.querySelector('body').classList.add('pop-over-open')
+    document.addEventListener('click', this.popOverClick)
+  }
+
+  popOverClick = (ev) => {
+    if (!this.popoverInner.contains(ev.target)) {
+      document.removeEventListener('click', this.popOverClick)
+      document.querySelector('body').classList.remove('pop-over-open')
+      this.setState({ buttonShown: false })
+    }
+  }
+
   render = () => {
-    const { id, name, email, status } = this.state
+    const { id, name, email, status, buttonShown } = this.state
     const { makeFriendRequest, confirmFriendRequest, searching } = this.props
+
+    const btnStates = [
+      { fn: makeFriendRequest, label: 'SEND FRIEND REQUEST', icon: 'fa fa-plus' },
+      { fn: confirmFriendRequest, label: 'APPROVE FRIEND REQUEST', icon: 'fa fa-check' }
+    ]
+
+    // button only makes requests or confirms
+
+    const btnState = searching && !status ? btnStates[0] :
+          !searching && status === 'pending' ? btnStates[1] :
+          null 
 
     // key is needed so SVG renders
 
     return (
       <div className='Friend'>
+        {/* <div className='pop-over'></div> */}
+
         <SVGInline svg={ jdenticon.toSvg(id, 50) } />
         <section>
           <h4>{ name }</h4>
@@ -45,11 +77,32 @@ class Friend extends Component {
           { searching && status === 'pending' ?
             <div key={ 4 }><i className='fa fa-hourglass-half'></i></div> :
             status === 'pending' ?
-            <div key={ 1 } onClick={ () => confirmFriendRequest(id) }><i className='fa fa-check'></i></div> :
+            <div key={ 1 } onClick={ this.showButton }><i className='fa fa-check'></i></div> :
             status === 'friends' ?
             <div key={ 2 }><i className='fa fa-ellipsis-h'></i></div> :
-            <div key={ 3 } onClick={ () => makeFriendRequest(id) }><i className='fa fa-plus'></i></div> }
+            <div key={ 3 } onClick={ this.showButton }><i className='fa fa-plus'></i></div> }
         </div>
+
+      { btnState && buttonShown ?
+        <div className='pop-over'>
+          <div className='inner' ref={ popoverInner => this.popoverInner = popoverInner }>
+            <div className='Friend'>
+              <SVGInline svg={ jdenticon.toSvg(id, 50) } />
+              <section>
+                <h4>{ name }</h4>
+                <p>{ email }</p>
+              </section>
+            </div>
+
+            <div className='action'>
+              <div><i className={btnState.icon}></i></div>
+            </div>
+
+            <Button
+              onClick={ () => btnState.fn(id) }
+              className={ `confirm-btn ${buttonShown ? 'shown' : ''}` }>{ btnState.label }</Button>
+          </div>
+        </div> : null }
       </div> 
     )
   }
